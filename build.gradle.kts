9b5e92f590d39db68172c05cc61468500b74219b
Icyrockton <42564541+Icyrockton@users.noreply.github.com>
2021-05-19 15:18:18 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.32"
+    id("org.jetbrains.compose") version "0.4.0-build188"
+    kotlin("plugin.serialization") version "1.4.30"
+
+}
+
+group = "com.icyrockton"
+version = "1.0"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("org.slf4j:slf4j-simple:1.7.30")
+    implementation("org.mockftpserver:MockFtpServer:2.8.0")
+    implementation("commons-net:commons-net:3.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
+
+
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "work2Collection"
+            packageVersion = "1.0.0"
+        }
+    }
+}
